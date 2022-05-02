20ff0c4b89a7748de56c147e4e279453e23373ba
Luisa Franco <lmfj95@gmail.com>
2022-05-02 17:24:48 -0500

    Taller 4 Analisis salarial completo

---
+++ taller4/salarios.js
@@ -0,0 +1,101 @@
+const colombia = [];
+
+colombia.push({
+ name: 'Luisa',
+ salary: 1000,
+});
+
+colombia.push({
+ name: 'Manuela',
+ salary: 900,
+});
+
+colombia.push({
+ name: 'Paulina',
+ salary: 500,
+});
+
+colombia.push({
+ name: 'Juan',
+ salary: 2000,
+});
+
+colombia.push({
+ name: 'Carlos',
+ salary: 300,
+});
+
+colombia.push({
+ name: 'Maria',
+ salary: 650,
+});
+
+colombia.push({
+ name: 'Fernanda',
+ salary: 850,
+});
+
+colombia.push({
+ name: 'Jose',
+ salary: 1500,
+});
+
+colombia.push({
+ name: 'Monica',
+ salary: 700,
+});
+
+colombia.push({
+ name: 'Pedro',
+ salary: 1100,
+});
+
+colombia.push({
+ name: 'Carlos',
+ salary: 480,
+});
+
+colombia.push({
+ name: 'Maria',
+ salary: 930,
+});
+
+colombia.push({
+ name: 'Luisa',
+ salary: 3000,
+});
+
+colombia.push({
+ name: 'Manuela',
+ salary: 3700,
+});
+
+colombia.push({
+ name: 'Paulina',
+ salary: 590,
+});
+
+colombia.push({
+ name: 'Juan',
+ salary: 5000,
+});
+
+colombia.push({
+ name: 'Carlos',
+ salary: 2800,
+});
+
+colombia.push({
+ name: 'Maria',
+ salary: 2100,
+});
+
+colombia.push({
+ name: 'Fernanda',
+ salary: 890,
+});
+
+colombia.push({
+ name: 'Jose',
+ salary: 1650,
+});
